0d321f9f17c80ca65ded242efe39dd890417bf8b
nhk9710 <hknoh@wkit.co.kr>
2023-10-30 15:28:16 +0900

    [feat]입력 로직 처리

--- s-num/src/App.vue
+++ s-num/src/App.vue
@@ -4,10 +4,39 @@ import { ref, onMounted } from 'vue'
 const inputNum = ref([0,0,0])
 const randomNum = ref([0,0,0])
 
+const chkAnswer = (() => {
+  let redCount = 0;
+  let yellowCount = 0;
+  let greenCount = 0;
+
+  let set = new Set(inputNum.value)
+
+  if(inputNum.value.length !== set.size){
+    alert('같은 숫자는 사용할 수 없어요~')
+    return false;
+  }
+
+
+  randomNum.value.forEach((v,i) => {
+    if(v === Number(inputNum.value[i])){
+      greenCount++;
+    }else if(randomNum.value.includes(Number(inputNum.value[i]))){
+      yellowCount++;
+    }else{
+      redCount++;
+    }
+  })
+});
+
+
 onMounted(() => {
-  randomNum.value[0] = Math.round(Math.random());
-  randomNum.value[1] = Math.floor(Math.random() * 10);
-  randomNum.value[2] = Math.floor(Math.random() * 10);
+  const allNumbers = Array.from({ length: 10 }, (_, index) => index);
+
+  for (let i = allNumbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allNumbers[i], allNumbers[j]] = [allNumbers[j], allNumbers[i]];
+  }
+  randomNum.value = allNumbers.slice(0, 3);
   alert('랜덤넘버는 ' + randomNum.value[0] + randomNum.value[1] + randomNum.value[2] + '입니다')
 })
 
@@ -24,17 +53,25 @@ onMounted(() => {
         <v-text-field class="mr-3"
                       @focus="inputNum[0] === 0 ? inputNum[0] = '' : ''"
                       @blur="!inputNum[0] ? inputNum[0] = 0 : ''"
+                      :rules="[val => val.length <= 1 ||  '1자리 이상 입력하지 마세요~' ]"
                       v-model="inputNum[0]" variant="solo-filled"></v-text-field>
         <v-text-field class="mr-3"
                       @focus="inputNum[1] === 0 ? inputNum[1] = '' : ''"
                       @blur="!inputNum[1] ? inputNum[1] = 0 : ''"
+                      :rules="[val => val.length <= 1 ||  '1자리 이상 입력하지 마세요~' ]"
                       v-model="inputNum[1]" variant="solo-filled"></v-text-field>
         <v-text-field class="mr-3"
                       @focus="inputNum[2] === 0 ? inputNum[2] = '' : ''"
                       @blur="!inputNum[2] ? inputNum[2] = 0 : ''"
+                      :rules="[val => val.length <= 1 ||  '1자리 이상 입력하지 마세요~' ]"
                       v-model="inputNum[2]" variant="solo-filled"></v-text-field>
         </div>
-        <v-btn variant="text" class="font-weight-bold text-h3">도전</v-btn>
+        <v-btn
+            :disabled=
+                "inputNum[0].length > 1 ||
+                 inputNum[1].length > 1 ||
+                 inputNum[2].length > 1"
+            variant="text" size="x-large" class="font-weight-bold text-h4" @click="chkAnswer">도전</v-btn>
       </div>
     </div>
 
@@ -56,7 +93,6 @@ onMounted(() => {
   height: 50%;
   margin: 0 auto;
   display: flex;
-  border: 1px solid black;
   align-items: center;
   flex-direction: column;
   justify-content: space-around;
